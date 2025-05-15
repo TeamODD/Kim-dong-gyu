b0d435f5db4b9a14bbf30c703e6de1381a53164b
1mxn107 <uu254664@gmail.com>
2025-05-15 23:56:31 +0900

    jot damn

---
+++ Kimdonggyeproject/Assets/DoorTrigger.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class DoorTrigger : MonoBehaviour
+{
+    public GameObject pressFPlane;
+    public SlideShowManager slideShowManager;
+    private bool isPlayerNearby = false;
+    private bool hasShownMessage = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = true;
+
+            if (!hasShownMessage)
+            {
+                pressFPlane.SetActive(true);
+                hasShownMessage = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNearby = false;
+            hasShownMessage = false;
+            pressFPlane.SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (isPlayerNearby && Input.GetKeyDown(KeyCode.F))
+        {
+            pressFPlane.SetActive(false); // 안내 메시지 숨기기
+            slideShowManager.StartSlideshow(); // 슬라이드쇼 실행
+        }
+    }
+}
